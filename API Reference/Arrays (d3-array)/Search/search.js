a516a79bff188b5202a93d874093f2aa89652053
ashwani-luhaniwal <contact.ashwin.cse@gmail.com>
2018-01-24 04:31:45 -0800

    D3 Array Search Methods

---
+++ API Reference/Arrays (d3-array)/Search/search.js
@@ -0,0 +1,15 @@
+/**
+ * ------------------------
+ * d3 Array Search Methods
+ * ------------------------
+ * Methods for searching arrays for a specific element.
+ *      d3.scan - linear search for an element using a comparator.
+ *      d3.bisect - binary search for a value in a sorted array.
+ *      d3.bisectRight - binary search for a value in a sorted array.
+ *      d3.bisectLeft - binary search for a value in a sorted array.
+ *      d3.bisector - bisect using an accessor or comparator.
+ *      bisector.left - bisectLeft, with the given comparator.
+ *      bisector.right - bisectRight, with the given comparator.
+ *      d3.ascending - compute the natural order of two values.
+ *      d3.descending - compute the natural order of two values.
+ */
